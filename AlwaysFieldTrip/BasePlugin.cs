35af01d3d8ec41782869840d32b212571de1c273
Denyscrasav4ik <horanden238@gmail.com>
2026-03-22 15:25:12 +0200

    Initial Commit

---
+++ AlwaysFieldTrip/BasePlugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+[BepInPlugin("basicallyukrainian.denyscrasav4ik.alwaysfieldtrip", "Always Field Trip", "1.0.0")]
+public class BasePlugin : BaseUnityPlugin
+{
+    void Awake()
+    {
+        var harmony = new Harmony("basicallyukrainian.denyscrasav4ik.alwaysfieldtrip");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(CoreGameManager))]
+public class CoreGameManagerPatch
+{
+    [HarmonyPatch("Update")]
+    [HarmonyPostfix]
+    static void AlwaysTripAvailable(CoreGameManager __instance)
+    {
+        __instance.tripAvailable = true;
+    }
+}
+
+// Patch 2: Fix the Generator Crash
+[HarmonyPatch(typeof(PitstopGameManager), "PrepareLevelData")]
+public class PitstopGeneratorPatch
+{
+    [HarmonyPostfix]
+    static void ForceTripGeneration(PitstopGameManager __instance, LevelData data)
+    {
+        var traverse = Traverse.Create(__instance);
+        var currentFieldTripField = traverse.Field("currentFieldTrip");
+
+        if (currentFieldTripField.GetValue() == null)
+        {
+            WeightedSelection<FieldTripObject>[] tierOneTrips = traverse.Field("tierOneTrips").GetValue<WeightedSelection<FieldTripObject>[]>();
+
+            if (tierOneTrips != null && tierOneTrips.Length > 0)
+            {
+                FieldTripObject forcedTrip = WeightedSelection<FieldTripObject>.RandomSelection(tierOneTrips);
+
+                currentFieldTripField.SetValue(forcedTrip);
+                data.roomAssetsPlacements.Add(forcedTrip.tripHub);
+
+                Singleton<CoreGameManager>.Instance.currentFieldTrip = forcedTrip;
+                Singleton<CoreGameManager>.Instance.tripAvailable = true;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Baldi's Basics Plus Always Field Trip
+
+## 📝 Functionality
+
+Basically, all this mod does is forces the **camping** field trip on every pit stop.
+
+## 📜 License
+
+This project is shared under the MIT License. Read LICENSE for more info.
